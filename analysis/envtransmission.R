9a92c7a7ee7e581ce02aa20bb4158665f132926b
Sophia Tan <sophiatan@berkeley.edu>
2022-12-06 14:20:34 -0800

    add variability to model

--- analysis/envtransmission.R
+++ analysis/envtransmission.R
@@ -5,14 +5,6 @@ library(purrr)
 
 setwd(here::here("results/separate_models/cumulative_inc_5/"))
 
-initial_pop <- data.frame(No=1:1000, 
-                          HH=rep(1:200, each=5), 
-                          S=1, 
-                          E=0, Ecounter=0, 
-                          I=0, Icounter=0, 
-                          R=0) # 200 households, 4 compartments (SEIR)
-results <- initial_pop[,1:2] %>% mutate(Itype=NA) # number of infections per household over time
-
 time <- 52*7 # num days to simulate (12 months total)
 months <- rep(1:13, each=30)[1:time]
 days_months <- data.frame(day=1:time, month=months)
@@ -21,30 +13,62 @@ days_months <- data.frame(day=1:time, month=months)
 num_weeks_inc = 4*7
 num_weeks_inf = 1*7
 
-SEIR_environment <- function(d, res, b, inc, inf) {
+SEIR_environment <- function(b, inc, inf) {
+  
+  # vary household size
+  # randomly sample household sizes such that total population is 1000 individuals
+  hh_size <- sample(x = c(3, 4, 5, 6), size=340, replace=T)
+  hh_size <- hh_size[which(cumsum(hh_size) < 1000)]
+  leftover <- 1000-sum(hh_size)
+  if(leftover < 3) {
+    hh <- 1:length(hh_size)
+    sampled <- sample(hh[hh_size<6], leftover)
+    hh_size[sampled] <- hh_size[sampled] + 1
+  } else {
+    hh_size <- c(hh_size, leftover)
+  }
+  
+  d <- data.frame(No=1:1000, 
+                  HHsize=rep(hh_size, times=hh_size),
+                  HH=rep(1:length(hh_size), times=hh_size), 
+                  S=1, 
+                  E=0, Ecounter=0, 
+                  I=0, Icounter=0, 
+                  R=0, 
+                  inc=0, inf=0) # variable number of households (size 3-6), total pop of 1000
+  res <- d[,1:3] %>% mutate(Itype=NA)
   
   for (i in 1:time) {
-    d$R[d$Icounter==inf] <- 1
-    d$I[d$Icounter==inf] <- 0
-    d$Icounter[d$Icounter==inf] <- 0
+    recovered <- d$inf>0 & d$Icounter==d$inf
+    if(sum(recovered,na.rm=T)>0) {
+      d$R[recovered] <- 1
+      d$I[recovered] <- 0
+      d$Icounter[recovered] <- 0 
+    }
     
-    d$I[d$Ecounter==inc] <- 1
-    d$Icounter[d$I==1] <- d$Icounter[d$I==1] + 1
-    d$E[d$Ecounter==inc] <- 0
-    d$Ecounter[d$Ecounter==inc] <- 0
+    new_inf <- d$inc>0 &d$Ecounter==d$inc
+    if(sum(new_inf,na.rm=T)>0) {
+      random_inf <- rnorm(sum(new_inf, na.rm=T), mean=inf, sd=1) %>% round()
+      d$I[new_inf] <- 1
+      d$inf[new_inf] <- random_inf
+      d$E[new_inf] <- 0
+      d$Ecounter[new_inf] <- 0 
+    }
     
-    summary_data <- d %>% group_by(HH) %>% 
-      mutate(Ih=sum(I)) %>% ungroup() %>% mutate(Ic=sum(I)-Ih) 
-    res <- res %>% cbind(I=summary_data$I)
+    res <- res %>% cbind(I=d$I)
     
     risk <- b*d$S
     
-    new_inf <- rbinom(1000, 1, risk)
+    new_exposed <- rbinom(nrow(d), 1, risk)
+
+    if(sum(new_exposed)>0) {
+      d$E[new_exposed==1] <- 1
+      d$inc[new_exposed==1] <- rnorm(sum(new_exposed, na.rm=T), mean=inc, sd=2) %>% round()
+    }
     
-    d$E[new_inf==1] <- 1
     d$Ecounter[d$E==1] <- d$Ecounter[d$E==1] + 1
+    d$Icounter[d$I==1] <- d$Icounter[d$I==1] + 1
     d$S[d$E==1] <- 0
-    
   }
   
   return(res)
@@ -52,22 +76,27 @@ SEIR_environment <- function(d, res, b, inc, inf) {
 
 
 beta <- 0.00016
+sims <- 1000
+num_hh <- rep(0, sims)
 
-for (i in 1:100) {
-  final <- SEIR_environment(initial_pop, results, beta, num_weeks_inc, num_weeks_inf)
+for (i in 1:sims) {
+  final <- SEIR_environment(beta, num_weeks_inc, num_weeks_inf)
+  num_hh[i] <- max(final$HH)
   
   # restructure table
-  names(final) <- c("No", "HH", "Type", 1:time)
-  f <- final %>% pivot_longer(cols = 4:ncol(.), names_to = "time") %>% mutate(time=as.numeric(time))
+  names(final) <- c("No", "HHsize", "HH", "Type", 1:time)
+  f <- final %>% pivot_longer(cols = 5:ncol(.), names_to = "time") %>% mutate(time=as.numeric(time))
   f <- f %>% group_by(No, value) %>% summarise_all(first) %>% filter(value==1) 
   f <- f %>% group_by(HH) %>% mutate(day_limits = list(time)) %>% 
     ungroup() %>% rowwise() %>% 
     mutate(has_hh = any((time - unlist(day_limits)) < 45 & (time - unlist(day_limits)) > 7)) %>% 
-    select(c(time, HH, Type, has_hh))
-   
+    select(c(time, HHsize, HH, Type, has_hh))
+  
   if(i==1){
+    if(nrow(f)==0) {next}
     inf_type <- cbind(i=i, f)
   }else{
+    if(nrow(f)==0) {next}
     inf_type <- rbind(inf_type, cbind(i=i, f))
   }
 }
@@ -75,11 +104,11 @@ for (i in 1:100) {
 inf_type <- inf_type %>% left_join(days_months, by=c("time"="day"))
 
 # average total infections
-(inf_type %>% nrow())/100
+(inf_type %>% nrow())/sims
 
 write_csv(inf_type, "simulated_data/environmental.csv")
 
-p <- inf_type %>% group_by(month, has_hh) %>% summarise(count=n()/100) %>%
+p <- inf_type %>% group_by(month, has_hh) %>% summarise(count=n()/sims) %>%
   ggplot() + geom_line(aes(month, count, group=has_hh, color=has_hh)) + 
   scale_color_discrete("Predicted source of infection", 
                        breaks=c(TRUE, FALSE),
@@ -87,16 +116,15 @@ p <- inf_type %>% group_by(month, has_hh) %>% summarise(count=n()/100) %>%
   scale_x_continuous("Time (months)") + 
   scale_y_continuous("Incidence (number of new infections)") + 
   labs(title="Incidence over time with predicted source of infection", 
-       subtitle="Only environmental source transmission, Cumulative incidence ~ 10%")
+       subtitle="Only environmental source transmission, Cumulative incidence ~ 5%")
 p %>% ggsave(filename = "figures/pred_environment.jpg")
 
 
-(inf_type %>% group_by(i) %>% summarise(prop=mean(has_hh)))$prop %>% mean()
+mean((inf_type %>% group_by(i) %>% summarise(HH=length(unique(HH)))/num_hh)$HH)
 
-# calculate average hh attack rate across hh and simulations
-inf_type %>% group_by(i, HH) %>% summarise(attack_rate_hh=n()/5) %>% 
-  group_by(i) %>% summarise(attack_rate=mean(attack_rate_hh)) %>% 
-  summary()
+(inf_type %>% group_by(i) %>% 
+    summarise(prop_pred=mean(has_hh))) %>% summary()
+
 
 # inf_type <- inf_type %>% group_by(i, month)
 # inf_type_check <- inf_type %>% summarise(has_hh = mean(has_hh,na.rm=T)) %>% 
