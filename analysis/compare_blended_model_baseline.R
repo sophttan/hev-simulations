97acc1df54abafe9360f0a4aa722f822e6b5ccdb
Sophia Tan <sophiatan@berkeley.edu>
2022-12-08 13:53:20 -0800

    blended model inc 5, hh risk 5

--- analysis/compare_blended_model_baseline.R
+++ analysis/compare_blended_model_baseline.R
@@ -3,12 +3,12 @@ gc()
 library(tidyverse)
 
 setwd(here::here("results/blended_model/cumulative_inc_5/"))
-hh31 <- read_csv("simulated_data/75p25e_hhrisk2.csv")
-hh11 <- read_csv("simulated_data/50p50e_hhrisk2.csv")
-hh13 <- read_csv("simulated_data/25p75e_hhrisk2.csv")
+hh31 <- read_csv("simulated_data/75p25e_hhrisk5.csv")
+hh11 <- read_csv("simulated_data/50p50e_hhrisk5.csv")
+hh13 <- read_csv("simulated_data/25p75e_hhrisk5.csv")
 
 env <- read_csv(here::here("results/separate_models/cumulative_inc_5/simulated_data/environmental.csv"))
-hh <- read_csv(here::here("results/separate_models/cumulative_inc_5/simulated_data/hh_risk2.csv"))
+hh <- read_csv(here::here("results/separate_models/cumulative_inc_5/simulated_data/hh_risk5.csv"))
 
 # months <- rep(1:13, each=35)[1:364]
 # days_months <- data.frame(day=1:364, month=months)
@@ -39,11 +39,11 @@ p <- inf_type %>% ggplot(aes(month)) +
   geom_line(data = hh_summ, aes(x=month, y=has_hh, color="Person-person only")) + 
   scale_x_continuous("Time (months)", breaks=1:13, expand=c(0,0)) +
   scale_y_continuous("Proportion of new cases with any household\ninfection in the last 7-45 days",
-                     expand=c(0.01, 0)) +
+                     expand=c(0.01, 0), limits = c(0,1)) +
   scale_color_brewer(palette="Dark2", direction=-1, 
                      name="Ratio of transmission\nfrom person-person contact\nand environmental source",
                      breaks=c("Person-person only", "75:25 ratio","50:50 ratio","25:75 ratio", "Environmental only")) +
   theme(panel.grid.minor = element_blank()) +
-  labs(title="Comparison of models at 5% cumulative incidence and\nhousehold relative risk of 2")
+  labs(title="Comparison of models at 5% cumulative incidence and\nhousehold relative risk of 5")
 p
-p %>% ggsave(filename = "figures/comparison_hhrisk2.jpg")
+p %>% ggsave(filename = "figures/comparison_hhrisk5.jpg")
