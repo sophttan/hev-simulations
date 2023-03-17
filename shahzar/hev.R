47e2bab68b2f0a3a20b89dbd3115a08724d0e732
Syed Mohammad Shahzar Rizvi <shahzar@arwen.Berkeley.EDU>
2023-03-17 16:15:51 +0000

    Small style changes

--- shahzar/hev.R
+++ shahzar/hev.R
@@ -6,183 +6,183 @@ inf = 7
 pop = 1000 # Population size
 
 create_hh <- function() {
-    # Randomly sample household sizes such that total population is 1000 individuals.
-    hh_size <- sample(x = c(3, 4, 5, 6), size = 340, replace = T)
-    
-    # Keep households such that total population is < 1000.
-    hh_size <- hh_size[which(cumsum(hh_size) < pop)]
+  # Randomly sample household sizes such that total population is 1000 individuals.
+  hh_size <- sample(x = c(3, 4, 5, 6), size = 340, replace = T)
   
-    leftover <- pop - sum(hh_size)
-    if (leftover < 3) {
-        hh <- 1:length(hh_size)
-        sampled <- sample(hh[hh_size < 6], leftover)
-        hh_size[sampled] <- hh_size[sampled] + 1
-    } else {
-        hh_size <- c(hh_size, leftover)
-    }
-    return(hh_size)
+  # Keep households such that total population is < 1000.
+  hh_size <- hh_size[which(cumsum(hh_size) < pop)]
+  
+  leftover <- pop - sum(hh_size)
+  if (leftover < 3) {
+    hh <- 1:length(hh_size)
+    sampled <- sample(hh[hh_size < 6], leftover)
+    hh_size[sampled] <- hh_size[sampled] + 1
+  } else {
+    hh_size <- c(hh_size, leftover)
+  }
+  return(hh_size)
 }
 
 SEIR <- function(beta_H, beta_C, inc, inf, verbose = 0) {
-    hh_size = create_hh()
-
-    # Create frame for running the simulation
-    # ID: ID of individual
-    # SIZE: size of individual's household
-    # HH: ID of individual's household
-    # S: susceptibility status
-    # E: exposed status
-    # E_count: number of days since exposed
-    # I: infectious status
-    # I_count: number of days since infectious
-    # R: recovered status
-    # INC: incubation period
-    # INF: infectious period
-    data = data.frame(ID = 1:pop,
-                      SIZE = rep(hh_size, times = hh_size),
-                      HH = rep(1:length(hh_size), times = hh_size), 
-                      S = c(0, rep(1, pop - 1)), 
-                      E = c(1, rep(0, pop - 1)),
-                      E_count = c(1, rep(0, pop - 1)), 
-                      I = 0,
-                      I_count = 0, 
-                      R = 0, 
-                      INC = c(round(rnorm(1, inc, 2)), rep(0, pop - 1)),
-                      INF = 0)
+  hh_size = create_hh()
   
-    # Create frame for storing results
-    # ID: ID of individual
-    # SIZE: size of individual's household
-    # HH: ID of individual's household
-    # TYPE: the kind of infection: household (H), community (C), or both (B)
-    # TIME: when the individual became infectious
-    # S_num: number of susceptible people in individual's household when their infectious period begins
-    # I_num: number of people in household that this individual infected over their infectious period
-    results <- data[, 1:3] %>% mutate(TYPE = NA, TIME = NA, S_num = NA, I_num = 0)
-    results$TYPE[1] = '0'
-    
-    for(t in 1:time) {
-        if (verbose) {
-            if (t %% 10 == 0) {
-                print(t)
-            }
-        }
-    
-        # Anyone who has been infectious for as many days as their infectious
-        # period is now recovered.
-        recovered <- (data$INF > 0) & (data$I_count == data$INF)
-        if(sum(recovered, na.rm = T) > 0) {
-            data$R[recovered] <- 1
-            data$I[recovered] <- 0
-            data$I_count[recovered] <- 0 
-        }
-    
-        # Anyone who has been incubating for as many days as their incubation
-        # period is now infectious.
-        new_inf <- (data$INC > 0) & (data$E_count == data$INC)
-        num_new_inf = sum(new_inf, na.rm = T)
-        if(num_new_inf > 0) {
-            # Change status to newly infectious and add infectious period.
-            data$I[new_inf] <- 1
-            random_inf <- rnorm(num_new_inf, mean = inf, sd = 1) %>% round()
-            data$INF[new_inf] <- random_inf
-            
-            # Remove exposure status and exposure count.
-            data$E[new_inf] <- 0
-            data$E_count[new_inf] <- 0 
-
-            # Record time at which infectious period starts.
-            results$TIME[new_inf == 1] <- t
-            
-            # Save the number of susceptible people in each infectious 
-            # individual's household.
-            S_data = data %>% group_by(HH) %>% mutate(S_tot = sum(S)) %>% select(HH, S_tot)
-            results$S_num[new_inf == 1] = S_data$S_tot[new_inf == 1]
-        }
-    
-        # I_H is the number of infections inside each household.
-        # I_C is the number of infections outside each household.
-        I_data <- data %>% group_by(HH) %>% 
-                           mutate(I_H = sum(I)) %>% 
-                           ungroup() %>% 
-                           mutate(I_C = sum(I) - I_H)
-    
-        # Calculate household risk and community risk.
-        risk_H <- beta_H * data$S * I_data$I_H / pop
-        risk_C <- beta_C * data$S * I_data$I_C / pop
-
-        # Each individual is infected from their household or 
-        # community independently with probabilities risk_H
-        # and risk_C.
-        new_inf_H <- rbinom(nrow(data), 1, risk_H)
-        new_inf_C <- rbinom(nrow(data), 1, risk_C)
-        
-        new_exposed = (new_inf_H == 1) | (new_inf_C == 1)
-        num_new_exposed = sum(new_exposed, na.rm = T)
-        if (sum(new_exposed) > 0) {
-            # Change status to newly exposed and add incubation period.
-            data$E[new_exposed] <- 1
-            random_inc <- rnorm(num_new_exposed, mean = inc, sd = 2) %>% round()
-            data$INC[new_exposed] <- random_inc
-            
-            # Remove susceptible status.
-            data$S[new_exposed] <- 0
-            
-            # Label community infections with C and household infections with H.
-            results$TYPE[new_inf_C == 1] <- 'C'
-            results$TYPE[new_inf_H == 1] <- 'H'
-            
-            # Get number of new infections in each household.
-            rr <- results %>% filter(new_inf_H == 1) %>%
-                              group_by(HH) %>%
-                              mutate(I_tot = sum(TYPE == 'H')) %>%
-                              ungroup()
-            rr <- unique(rr[, c('HH', 'I_tot')])
-            
-            # Get people with the smallest I_counts and the households with the
-            # new infections.
-            dd = data %>% filter(data$I == 1) %>% group_by(HH) %>% slice(which.min(I_count))
-            min_IDs <- dd$ID
-            new_HHs <- results[new_inf_H == 1, ]$HH
-            
-            # Every individual with the smallest I_count in each household with 
-            # at least one new H infection gets the number of new H infections
-            # added to their I_num.
-            idx <- (data$ID %in% min_IDs) & (data$HH %in% new_HHs)
-            
-            # For debugging purposes.
-            if (length(results[idx, ]$I_num) != length(rr$I_tot)) {
-                return(list(data, results, new_inf_H))
-            }
-            
-            results[idx, ]$I_num <- results[idx, ]$I_num + rr$I_tot
-            
-            # Label individuals with both a household and community infection with B.
-            results$TYPE[(new_inf_H == 1) & (new_inf_C == 1)] <- 'B'
-        }
-        # Increment exposure and infectious counters.
-        data$E_count[data$E == 1] <- data$E_count[data$E == 1] + 1
-        data$I_count[data$I == 1] <- data$I_count[data$I == 1] + 1
+  # Create frame for running the simulation
+  # ID: ID of individual
+  # SIZE: size of individual's household
+  # HH: ID of individual's household
+  # S: susceptibility status
+  # E: exposed status
+  # E_count: number of days since exposed
+  # I: infectious status
+  # I_count: number of days since infectious
+  # R: recovered status
+  # INC: incubation period
+  # INF: infectious period
+  data = data.frame(ID = 1:pop,
+                    SIZE = rep(hh_size, times = hh_size),
+                    HH = rep(1:length(hh_size), times = hh_size), 
+                    S = c(0, rep(1, pop - 1)), 
+                    E = c(1, rep(0, pop - 1)),
+                    E_count = c(1, rep(0, pop - 1)), 
+                    I = 0,
+                    I_count = 0, 
+                    R = 0, 
+                    INC = c(round(rnorm(1, inc, 2)), rep(0, pop - 1)),
+                    INF = 0)
+  
+  # Create frame for storing results
+  # ID: ID of individual
+  # SIZE: size of individual's household
+  # HH: ID of individual's household
+  # TYPE: the kind of infection: household (H), community (C), or both (B)
+  # TIME: when the individual became infectious
+  # S_num: number of susceptible people in individual's household when their infectious period begins
+  # I_num: number of people in household that this individual infected over their infectious period
+  results <- data[, 1:3] %>% mutate(TYPE = NA, TIME = NA, S_num = NA, I_num = 0)
+  results$TYPE[1] = '0'
+  
+  for(t in 1:time) {
+    if (verbose) {
+      if (t %% 10 == 0) {
+        print(t)
+      }
     }
-    return(results)
+    
+    # Anyone who has been infectious for as many days as their infectious
+    # period is now recovered.
+    recovered <- (data$INF > 0) & (data$I_count == data$INF)
+    if(sum(recovered, na.rm = T) > 0) {
+      data$R[recovered] <- 1
+      data$I[recovered] <- 0
+      data$I_count[recovered] <- 0 
+    }
+    
+    # Anyone who has been incubating for as many days as their incubation
+    # period is now infectious.
+    new_inf <- (data$INC > 0) & (data$E_count == data$INC)
+    num_new_inf = sum(new_inf, na.rm = T)
+    if(num_new_inf > 0) {
+      # Change status to newly infectious and add infectious period.
+      data$I[new_inf] <- 1
+      random_inf <- rnorm(num_new_inf, mean = inf, sd = 1) %>% round()
+      data$INF[new_inf] <- random_inf
+      
+      # Remove exposure status and exposure count.
+      data$E[new_inf] <- 0
+      data$E_count[new_inf] <- 0 
+      
+      # Record time at which infectious period starts.
+      results$TIME[new_inf == 1] <- t
+      
+      # Save the number of susceptible people in each infectious 
+      # individual's household.
+      S_data = data %>% group_by(HH) %>% mutate(S_tot = sum(S)) %>% select(HH, S_tot)
+      results$S_num[new_inf == 1] = S_data$S_tot[new_inf == 1]
+    }
+    
+    # I_H is the number of infections inside each household.
+    # I_C is the number of infections outside each household.
+    I_data <- data %>% group_by(HH) %>% 
+      mutate(I_H = sum(I)) %>% 
+      ungroup() %>% 
+      mutate(I_C = sum(I) - I_H)
+    
+    # Calculate household risk and community risk.
+    risk_H <- beta_H * data$S * I_data$I_H / pop
+    risk_C <- beta_C * data$S * I_data$I_C / pop
+    
+    # Each individual is infected from their household or 
+    # community independently with probabilities risk_H
+    # and risk_C.
+    new_inf_H <- rbinom(nrow(data), 1, risk_H)
+    new_inf_C <- rbinom(nrow(data), 1, risk_C)
+    
+    new_exposed = (new_inf_H == 1) | (new_inf_C == 1)
+    num_new_exposed = sum(new_exposed, na.rm = T)
+    if (sum(new_exposed) > 0) {
+      # Change status to newly exposed and add incubation period.
+      data$E[new_exposed] <- 1
+      random_inc <- rnorm(num_new_exposed, mean = inc, sd = 2) %>% round()
+      data$INC[new_exposed] <- random_inc
+      
+      # Remove susceptible status.
+      data$S[new_exposed] <- 0
+      
+      # Label community infections with C and household infections with H.
+      results$TYPE[new_inf_C == 1] <- 'C'
+      results$TYPE[new_inf_H == 1] <- 'H'
+      
+      # Get number of new infections in each household.
+      rr <- results %>% filter(new_inf_H == 1) %>%
+        group_by(HH) %>%
+        mutate(I_tot = sum(TYPE == 'H')) %>%
+        ungroup()
+      rr <- unique(rr[, c('HH', 'I_tot')])
+      
+      # Get people with the smallest I_counts and the households with the
+      # new infections.
+      dd = data %>% filter(data$I == 1) %>% group_by(HH) %>% slice(which.min(I_count))
+      min_IDs <- dd$ID
+      new_HHs <- results[new_inf_H == 1, ]$HH
+      
+      # Every individual with the smallest I_count in each household with 
+      # at least one new H infection gets the number of new H infections
+      # added to their I_num.
+      idx <- (data$ID %in% min_IDs) & (data$HH %in% new_HHs)
+      
+      # For debugging purposes.
+      if (length(results[idx, ]$I_num) != length(rr$I_tot)) {
+        return(list(data, results, new_inf_H))
+      }
+      
+      results[idx, ]$I_num <- results[idx, ]$I_num + rr$I_tot
+      
+      # Label individuals with both a household and community infection with B.
+      results$TYPE[(new_inf_H == 1) & (new_inf_C == 1)] <- 'B'
+    }
+    # Increment exposure and infectious counters.
+    data$E_count[data$E == 1] <- data$E_count[data$E == 1] + 1
+    data$I_count[data$I == 1] <- data$I_count[data$I == 1] + 1
+  }
+  return(results)
 }
 
 metrics = function(results) {
-    # Incidence is the proportion of the population that became infected.
-    idc = mean(!is.na(results$TYPE))
-    
-    # If incidence is 0, the SAR is undefined.
-    sar = NA
-    if (idc != 0) {
-      # The SAR is the average SAR for each individual that was infectious.
-      sar = mean(results$I_num / results$S_num, na.rm = T)
-    }
-    return(c(idc, sar))
+  # Incidence is the proportion of the population that became infected.
+  idc = mean(!is.na(results$TYPE))
+  
+  # If incidence is 0, the SAR is undefined.
+  sar = NA
+  if (idc != 0) {
+    # The SAR is the average SAR for each individual that was infectious.
+    sar = mean(results$I_num / results$S_num, na.rm = T)
+  }
+  return(c(idc, sar))
 }
 
 score = function(obs, target) {
-    # The score is the L2 distance of the observed values from the target.
-    return(sum((obs - target)^2))
+  # The score is the L2 distance of the observed values from the target.
+  return(sum((obs - target)^2))
 }
 
 # The likelihood is calculated by first averaging the incidence and SAR over N
@@ -280,7 +280,6 @@ best = metropolis_results[[3]]
 save(chain, file = "chain.Rdata")
 save(liks, file = "liks.Rdata")
 save(best, file = "best.Rds")
-
 
 beta_H = 30
 beta_C = 0.15
