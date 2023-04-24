abfed1665f200cdfb29d302d5e6ec8b3b12a2d0b
Sophia Tan <sophiatan@berkeley.edu>
2023-04-24 13:24:26 -0700

    calibration

--- analysis/hand_calibration.R
+++ analysis/hand_calibration.R
@@ -8,18 +8,17 @@ source(here::here("analysis/functions/seir_functions.R"))
 #### SEIR Simulation ####
 #########################
 time <- 365 # Number of days.
-inc <- 28 # Average incubation period length.
 inf <- 7 # Average infectious period length.
 N <- 1000 # Population size.
 
-start <- c(53.6136261376403, 0.086607502588279)
+start <- c(53.8, 0.1)
 incidence<-NULL
 sar<-NULL
 results_data<-NULL
 
 sims <- 100
 for (i in 1:sims) {
-  results <- SEIR(start, inc, inf)
+  results <- SEIR(start, inf)
   res <- metrics(results)
   incidence<-c(incidence,res[1])
   sar<-c(sar,res[2])
