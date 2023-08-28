bc64a60640cc6fe20cfcd294e19b47a1ba0ff4d7
Shahzar <shahzar@dorothy.Berkeley.EDU>
2023-08-28 06:15:33 -0700

    no longini

--- shahzar/results/48/panmictic/analysis.R
+++ shahzar/results/48/panmictic/analysis.R
@@ -28,7 +28,7 @@ method_HH <- function(results) {
   return(mean(f$IS_H))
 }
 
-probability <- function(cases, index, rel_p_hh=1) {
+probability <- function(cases, index, rel_p_hh = 1) {
   inc_prim <- cases$TIME
   inc_sec <- cases$TIME[index]
   hh_prim <- cases$HH
@@ -45,6 +45,65 @@ probability <- function(cases, index, rel_p_hh=1) {
   return(results/sum(results))
 }
 
+proportion_HH <- function(results, relative = T){
+  sim_data <- results %>% filter(!is.na(TIME))
+  sim_data_HH <- results %>% select(HH, SIZE) %>% distinct(HH, .keep_all = T)
+  
+  # household sizes in the data
+  k <- sort(unique(sim_data_HH$SIZE))
+  # how many households of each size
+  n <- table(sim_data_HH$SIZE)
+  # matrix with rows=number of infections, cols=household size
+  a <- matrix(0,nrow=max(k)+1,ncol=length(k))
+  if (length(unique(sim_data$HH))>1){
+    cases_per_HH <- table(sim_data$HH,sim_data$SIZE)
+  } else {
+    cases_per_HH <- table(sim_data$HH)
+    cases_per_HH <- t(cases_per_HH)
+    colnames(cases_per_HH) <- unique(sim_data$SIZE)[[1]]
+  }
+  for (idx in 1:length(k)){
+    size <- k[idx]
+    if (as.character(size) %in% colnames(cases_per_HH)){
+      case_counts <- table(cases_per_HH[,as.character(size)])
+      for (cases in labels(case_counts)[[1]]){
+        a[as.integer(cases)+1,idx] <- case_counts[cases]
+      }
+    }
+  }
+  # above loop gives incorrect zero-cases, instead find zero-case
+  # households by finding households not included in case data
+  a[1,] <- 0
+  for (idx in 1:length(k)){
+    size <- k[idx]
+    for (HH in sim_data_HH[sim_data_HH$SIZE==size,]$HH){
+      if (!(HH %in% sim_data$HH)){
+        a[1,idx] <- a[1,idx] + 1
+      }
+    }
+  }
+  # B_est is probability of escaping infection from community transmission
+  B_est <- sum(n*(a[1,]/n)**(1/k))/sum(n)
+  
+  # phi is avg number infected per household
+  phi <- sum((0:max(k))*rowSums(a))/sum(n)
+  # theta is the household attack rate
+  theta <- sum((0:max(k))*rowSums(t(t(a)/k)))/sum(n)
+  # Q_est is probability of escaping infection from household transmission
+  # estimator can give probabilities greater than 1 so set max of 1
+  Q_est <- min(1,((1-theta)/B_est)**(1/phi))
+  
+  # give relative probability of household infectiohn
+  if (relative){
+    return((1-Q_est)/(1-B_est))
+  }
+  
+  # proportion of household infection expressed as conditional probability
+  # of household infection given infection at all
+  p_HH <- (1-Q_est)/(1-B_est*Q_est)
+  return(p_HH)
+}
+
 method_R <- function(results) {
   cases <- results %>% 
     filter(!is.na(TIME)) %>%
@@ -54,7 +113,8 @@ method_R <- function(results) {
   R_HH <- rep(0, nrow(cases))
   
   for (k in 1:nrow(cases)) {
-    probs <- probability(cases, k, 1)
+    rel_p_hh <- proportion_HH(results)
+    probs <- probability(cases, k, rel_p_hh)
     HH_probs <- probs
     HH_probs[cases$HH != cases$HH[k]] <- 0
     R <- R + probs
@@ -64,7 +124,7 @@ method_R <- function(results) {
 }
 
 values <- function(results) {
-  return(method_R(results))
+  return(c(prp_true(results), method_HH(results), method_R(results)))
 }
 
 # 5% Cumulative Incidence
@@ -74,8 +134,8 @@ vals <- foreach (i = 0:n_sims, .combine = 'c') %dopar% {
   values(results)
 }
 vals <- matrix(vals, n_sims + 1, byrow = T)
-saveRDS(vals, file = '5/no_longini.rds')
-write.table(vals, file = '5/no_longini.txt', row.names = F, col.names = F)
+saveRDS(vals, file = '5/vals.rds')
+write.table(vals, file = '5/vals.txt', row.names = F, col.names = F)
 
 
 # 10% Cumulative Incidence
@@ -85,8 +145,8 @@ vals <- foreach (i = 0:n_sims, .combine = 'c') %dopar% {
   values(results)
 }
 vals <- matrix(vals, n_sims + 1, byrow = T)
-saveRDS(vals, file = '10/no_longini.rds')
-write.table(vals, file = '10/no_longini.txt', row.names = F, col.names = F)
+saveRDS(vals, file = '10/vals.rds')
+write.table(vals, file = '10/vals.txt', row.names = F, col.names = F)
 
 
 # 30% Cumulative Incidence
@@ -96,7 +156,5 @@ vals <- foreach (i = 0:n_sims, .combine = 'c') %dopar% {
   values(results)
 }
 vals <- matrix(vals, n_sims + 1, byrow = T)
-saveRDS(vals, file = '30/no_longini.rds')
-write.table(vals, file = '30/no_longini.txt', row.names = F, col.names = F)
-
-
+saveRDS(vals, file = '30/vals.rds')
+write.table(vals, file = '30/vals.txt', row.names = F, col.names = F)
