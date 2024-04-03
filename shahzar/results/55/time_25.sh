8a738323e84ac7e8d4c0a1c92916fd8c01f9affe
Shahzar <shahzar@scf.berkeley.edu>
2024-04-03 03:32:53 -0700

    Cleanup

--- shahzar/results/55/time_25.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
-#SBATCH --nodes=1
-
-#SBATCH --mail-user=shahzar@berkeley.edu
-#SBATCH --mail-type=ALL
-
-R CMD BATCH --no-save time_25.R time_25.out
--- shahzar/results/55/time_pan.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=24
-#SBATCH --nodes=1
-
-#SBATCH --mail-user=shahzar@berkeley.edu
-#SBATCH --mail-type=ALL
-
-R CMD BATCH --no-save time_pan.R time_pan.out
